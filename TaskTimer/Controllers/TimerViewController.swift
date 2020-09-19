c6b5d35472ef6f8bd5fa3a2ff334c9296bbb26af
Matt <mcarter91@gmail.com>
2020-09-20 00:12:59 +1000

    timer UI refactor 1

--- TaskTimer/Controllers/TimerViewController.swift
+++ TaskTimer/Controllers/TimerViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class TimerViewController: UIViewController {
 
+    @IBOutlet weak var lapsedTimeLabel: UILabel!
+    var timer = Timer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +21,5 @@ class TimerViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
