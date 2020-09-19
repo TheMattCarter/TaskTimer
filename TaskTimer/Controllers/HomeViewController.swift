bba93d6f54874c1cfe00a70f8226d957e8d3a6a8
Matt <mcarter91@gmail.com>
2020-09-19 16:35:05 +1000

    initial commit

---
+++ TaskTimer/Controllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskTimer
+//
+//  Created by Matthew Carter on 9/19/20.
+//  Copyright © 2020 Matthew Carter. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
