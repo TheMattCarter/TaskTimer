806e6e99085a442d5cbd978fdd2e678da188fc6c
Matt <mcarter91@gmail.com>
2020-09-20 22:39:12 +1000

    interval timer update

--- TaskTimer/Controllers/TimerSelectViewController.swift
+++ TaskTimer/Controllers/TimerSelectViewController.swift
@@ -10,37 +10,77 @@ import UIKit
 
 class TimerSelectViewController: UIViewController {
 
+    
+    @IBOutlet weak var basicCountDownTime: UIDatePicker!
+    @IBOutlet weak var workCountDownTime: UIDatePicker!
+    @IBOutlet weak var breakCountDownTime: UIDatePicker!
+    @IBOutlet weak var qtyIntervalsLabel: UILabel!
+    @IBOutlet weak var qtyIntevalsStepper: UIStepper!
+    var selectedInitialTime = 0
+    var restTime = 0
+    @IBOutlet weak var intervalLabel: UILabel!
+    var qtyIntervals = 4
+    var mode = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        basicCountDownTime.minuteInterval = 5
+        workCountDownTime.minuteInterval = 1
+        breakCountDownTime.minuteInterval = 1
+        
     }
     
 
+    @IBAction func updateIntervalLabel(_ sender: Any) {
+        intervalLabel.text = "(\(String(format: "%.0f", qtyIntevalsStepper.value)))"
+    }
+    
+    //String(format: “%.2f”, inputValue)
+    
+    @IBAction func startCountDown(_ sender: UIDatePicker) {
+        selectedInitialTime = Int(basicCountDownTime.countDownDuration)
+       restTime = 0
+       qtyIntervals = 0
+        mode = 2
+        
+        goToTimer()
+    }
+    
+    @IBAction func startIntervals(_ sender: Any) {
+        selectedInitialTime = Int(workCountDownTime.countDownDuration)
+        restTime = Int(breakCountDownTime.countDownDuration)
+        qtyIntervals = Int(qtyIntevalsStepper.value)
+        mode = 1
+        
+        goToTimer()
+    }
+    
     @IBAction func startRunningTime(_ sender: Any) {
-        startNewRunningTimer()
+        selectedInitialTime = 0
+        restTime = 0
+        qtyIntervals = 0
+        mode = 0
+        
+        goToTimer()
     }
     
-    func startNewRunningTimer() {
+    func goToTimer() {
+        
+        print(selectedInitialTime)
+        
         performSegue(withIdentifier: "goToTimer", sender: self)
         
-        func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            let destinationVC = segue.destination as! TimerViewController
-            
-            destinationVC.initialTime = 0
-            
-        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TimerViewController
+        
+        destinationVC.timerCurrentTime = self.selectedInitialTime
+        destinationVC.timerRestTime = self.restTime
+        destinationVC.timerQtyIntervals = self.qtyIntervals
+        destinationVC.timerMode = self.mode
         
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- TaskTimer/Controllers/TimerViewController.swift
+++ TaskTimer/Controllers/TimerViewController.swift
@@ -10,43 +10,156 @@ import UIKit
 
 class TimerViewController: UIViewController {
 
-    @IBOutlet weak var lapsedTimeLabel: UILabel!
+    
     var timer = Timer()
-    var initialTime = 0
-    var secondsPassed = 0
+    var timerCurrentTime = 0
+    var timerRestTime = 0
+    var timerQtyIntervals = 0
+    var timerMode = 0
+    var secondsLapsedWorking = 0
+    var secondsLapsedResting = 0
+    @IBOutlet weak var modeLabel: UILabel!
+    
     @IBOutlet weak var timeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        countUp()
-        // Do any additional setup after loading the view.
-    }
-    
-    func countUp() {
-        timer.invalidate()
-        secondsPassed = 0
+        print(timerCurrentTime)
         
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateLapsedTime), userInfo: nil, repeats: true)
+        switch timerMode{
+        case 0: startRunningTime()
+        case 1: startIntervals()
+        case 2: startCountDown()
+        default: print("error selecting timer mode")
+        }
         
     }
 
-    func countDown() {
+    func startCountDown() {
+        print ("countdown")
+        modeLabel?.text = "insert inspirational quote here"
+        timer.invalidate()
+        secondsLapsedWorking = 0
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(descendingTimer), userInfo: nil, repeats: true)
         
     }
     
-    @objc func updateLapsedTime() {
-        secondsPassed += 1
-        print(secondsPassed)
-        updateTimeLabel()
+    func startIntervals() {
+        let intervalsToExecute = timerQtyIntervals
+        let selectedWorkTime = timerCurrentTime
+        
+        print("interval")
+                
+        for i in 0..<(2 * timerQtyIntervals){
+            
+            if timerCurrentTime > 0 {
+                
+                print("period \((i/2) + 1) of \(intervalsToExecute)")
+                
+                timer.invalidate()
+                print("work")
+                modeLabel.text = "work"
+                //timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(descendingTimer), userInfo: nil, repeats: true)
+                
+                timerCurrentTime = 0
+            } else {
+                timer.invalidate()
+                print("break")
+                modeLabel.text = "break"
+                //timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(intBreakTimer), userInfo: nil, repeats: true)
+                timerCurrentTime = selectedWorkTime
+            }
+
+        timerQtyIntervals -= 1
+        }
+        
     }
     
-    func updateTimeLabel() {
+    func startRunningTime() {
+           print("running time")
+        modeLabel?.text = "insert another inspirational quote here"
+           timer.invalidate()
+           secondsLapsedWorking = 0
+           
+           timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ascendingTimer), userInfo: nil, repeats: true)
+           
+       }
+    
+    @objc func descendingTimer() {
+        secondsLapsedWorking += 1
+        timerCurrentTime -= 1
+        print (timerCurrentTime)
+        updateTimeLabel(timerLabelSeconds: timerCurrentTime)
+    }
+    
+    @objc func ascendingTimer() {
+           secondsLapsedWorking += 1
+           timerCurrentTime += 1
+           print(timerCurrentTime)
+        updateTimeLabel(timerLabelSeconds: timerCurrentTime)
+       }
+    
+    @objc func intBreakTimer() {
+        secondsLapsedResting += 1
+        timerRestTime -= 1
+        print(timerRestTime)
+        updateTimeLabel(timerLabelSeconds: timerRestTime)
+
+    }
+    
+//    @objc func intTimer() {
+//
+//        let selectedWorkTime = timerCurrentTime
+//
+//        if timer.timeinterval < selectedWorkTime {
+//            print("work")
+//            modeLabel.text = "work"
+//            secondsLapsedWorking += 1
+//            timerCurrentTime -= 1
+//            print(timerCurrentTime)
+//            updateTimeLabel(timerLabelSeconds: timerCurrentTime)
+//            // timerCurrentTime = 0
+//
+//        } else {
+//            print("break")
+//            modeLabel.text = "break"
+//            secondsLapsedResting += 1
+//            timerRestTime -= 1
+//            print(timerRestTime)
+//            updateTimeLabel(timerLabelSeconds: timerRestTime)
+//            timerCurrentTime = selectedWorkTime
+//        }
+//
+//    }
+
+    
+    
+    func updateTimeLabel(timerLabelSeconds: Int) {
+
         let timeFormatter = DateComponentsFormatter()
         timeFormatter.allowedUnits = [.hour, .minute, .second]
         timeFormatter.unitsStyle = .positional
         timeFormatter.zeroFormattingBehavior = .pad
+
+        let formattedTimeValue = timeFormatter.string(from: TimeInterval(timerLabelSeconds))
         
-        let formattedTimeValue = timeFormatter.string(from: TimeInterval(secondsPassed))
         timeLabel.text = formattedTimeValue
+        
+    }
+    
+    
+    @IBAction func pausePressed(_ sender: Any) {
+        stopTimer()
+    }
+    
+    @IBAction func donePressed(_ sender: Any) {
+        stopTimer()
+    }
+    
+    
+    func stopTimer() {
+        print(secondsLapsedWorking)
+        timer.invalidate()
     }
 }
